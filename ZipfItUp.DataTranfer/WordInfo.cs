e383964226fae05ce0817fedb534445e6c897555
MrSpecialR <avatarthegame35@gmail.com>
2017-04-17 14:42:05 +0300

    Set up basic chart visualization

---
+++ ZipfItUp.DataTranfer/WordInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZipfItUp.Models;
+using Newtonsoft.Json;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Newtonsoft.Json.Linq;
+
+namespace ZipfItUp.DataTranfer
+{
+    public class WordInfo
+    {
+        
+        public WordInfo(List<Word> words)
+        {
+           // Mapper.Initialize(x => x.CreateMap<Word, WordDTO>());
+           this.Words = Mapper.Map<Word[], List<WordDTO>>(words.ToArray()).ToList();
+           // this.WordsJSON = JsonConvert.SerializeObject(this.Words);
+            string JsonObject = JsonConvert.SerializeObject(this.Words);
+            JArray obj = JArray.Parse(JsonObject);
+            JArray labels = new JArray();
+            JArray data = new JArray();
+            foreach (JObject currObj in obj)
+            {
+                labels.Add(currObj["WordString"]);
+                data.Add(currObj["Occurances"]);
+            }
+            this.WordsJSON = labels.ToString(Formatting.None);
+            this.OccurancesJSON = data.ToString(Formatting.None);
+        } 
+
+        public List<WordDTO> Words { get; set; }
+        public string WordsJSON { get; set; }
+        public string OccurancesJSON { get; set; }
+    }
+
+    public class WordDTO
+    {
+        public int Id { get; set; }
+        public string WordString { get; set; }
+        public long Occurances{ get; set; }
+    }
+}
--- ZipfItUp/Global.asax.cs
+++ ZipfItUp/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using ZipfItUp.DataTranfer;
+using ZipfItUp.Models;
 
 namespace ZipfItUp
 {
@@ -16,6 +19,7 @@ namespace ZipfItUp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Mapper.Initialize(config => config.CreateMap<Word, WordDTO>());
         }
     }
 }
